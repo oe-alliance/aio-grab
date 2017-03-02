054164c23e83e0d637fadc2f73d8cb9ef702a860
arn354 <arn354@email.de>
2017-03-02 21:24:01 +0100

    add BRCM7252S

--- main.c
+++ main.c
@@ -154,9 +154,9 @@ void (*resize)(const unsigned char *source, unsigned char *dest, int xsource, in
 void combine(unsigned char *output, const unsigned char *video, const unsigned char *osd, int vleft, int vtop, int vwidth, int vheight, int xres, int yres);
 
 #if !defined(__sh__)
-static enum {UNKNOWN, WETEK, AZBOX863x, AZBOX865x, PALLAS, VULCAN, XILLEON, BRCM7400, BRCM7401, BRCM7405, BRCM7325, BRCM7335, BRCM7346, BRCM7358, BRCM7362, BRCM7241, BRCM7251, BRCM7252, BRCM7356, BRCM7424, BRCM7425, BRCM7435, BRCM7444, BRCM7552, BRCM7581, BRCM7583, BRCM7584, BRCM7366, BRCM73625, BRCM73565, BRCM7439} stb_type = UNKNOWN;
+static enum {UNKNOWN, WETEK, AZBOX863x, AZBOX865x, PALLAS, VULCAN, XILLEON, BRCM7400, BRCM7401, BRCM7405, BRCM7325, BRCM7335, BRCM7346, BRCM7358, BRCM7362, BRCM7241, BRCM7251, BRCM7252, BRCM7252S, BRCM7356, BRCM7424, BRCM7425, BRCM7435, BRCM7444, BRCM7552, BRCM7581, BRCM7583, BRCM7584, BRCM7366, BRCM73625, BRCM73565, BRCM7439} stb_type = UNKNOWN;
 #else
-static enum {UNKNOWN, WETEK, AZBOX863x, AZBOX865x, ST, PALLAS, VULCAN, XILLEON, BRCM7400, BRCM7401, BRCM7405, BRCM7325, BRCM7335, BRCM7346, BRCM7358, BRCM7362, BRCM7241, BRCM7251, BRCM7252, BRCM7356, BRCM7424, BRCM7425, BRCM7435, BRCM7444, BRCM7552, BRCM7581, BRCM7583, BRCM7584, BRCM7366, BRCM73625, BRCM73565, BRCM7439} stb_type = UNKNOWN;
+static enum {UNKNOWN, WETEK, AZBOX863x, AZBOX865x, ST, PALLAS, VULCAN, XILLEON, BRCM7400, BRCM7401, BRCM7405, BRCM7325, BRCM7335, BRCM7346, BRCM7358, BRCM7362, BRCM7241, BRCM7251, BRCM7252, BRCM7252S, BRCM7356, BRCM7424, BRCM7425, BRCM7435, BRCM7444, BRCM7552, BRCM7581, BRCM7583, BRCM7584, BRCM7366, BRCM73625, BRCM73565, BRCM7439} stb_type = UNKNOWN;
 #endif
 
 static int chr_luma_stride = 0x40;
@@ -284,6 +284,11 @@ int main(int argc, char **argv)
 				else if (strstr(buf,"7252"))
 				{
 					stb_type = BRCM7252;
+					break;
+				}
+				else if (strstr(buf,"7252S"))
+				{
+					stb_type = BRCM7252S;
 					break;
 				}
 				else if (strstr(buf,"73565"))
@@ -440,6 +445,7 @@ int main(int argc, char **argv)
 		case BRCM7552:
 		case BRCM7251:
 		case BRCM7252:
+		case BRCM7252S:
 		case BRCM7581:
 			registeroffset = 0x10600000;
 			chr_luma_stride = 0x40;
@@ -571,7 +577,7 @@ int main(int argc, char **argv)
 	{
 		if (!quiet)
 			fprintf(stderr, "Grabbing Video ...\n");
-		if (stb_type == BRCM7366 || stb_type == BRCM7251 || stb_type == BRCM7252 || stb_type == BRCM7444)
+		if (stb_type == BRCM7366 || stb_type == BRCM7251 || stb_type == BRCM7252 || stb_type == BRCM7252S || stb_type == BRCM7444)
 		{
 			getvideo2(video, &xres_v,&yres_v);
 		}
