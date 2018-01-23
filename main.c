84041846cfeb190f1ebf328a5012dac28d102087
captain <openatv@gmail.com>
2018-01-23 16:37:34 +0100

    add hisilicon hi3798*v*00

--- main.c
+++ main.c
@@ -154,9 +154,9 @@ void (*resize)(const unsigned char *source, unsigned char *dest, int xsource, in
 void combine(unsigned char *output, const unsigned char *video, const unsigned char *osd, int vleft, int vtop, int vwidth, int vheight, int xres, int yres);
 
 #if !defined(__sh__)
-static enum {UNKNOWN, WETEK, AZBOX863x, AZBOX865x, PALLAS, VULCAN, XILLEON, BRCM7400, BRCM7401, BRCM7405, BRCM7325, BRCM7335, BRCM7346, BRCM7358, BRCM7362, BRCM7241, BRCM7251, BRCM7252, BRCM7252S, BRCM7356, BRCM7424, BRCM7425, BRCM7435, BRCM7444, BRCM7552, BRCM7581, BRCM7583, BRCM7584, BRCM72604, BRCM75845, BRCM7366, BRCM73625, BRCM73565, BRCM7439} stb_type = UNKNOWN;
+static enum {UNKNOWN, WETEK, AZBOX863x, AZBOX865x, PALLAS, VULCAN, XILLEON, BRCM7400, BRCM7401, BRCM7405, BRCM7325, BRCM7335, BRCM7346, BRCM7358, BRCM7362, BRCM7241, BRCM7251, BRCM7252, BRCM7252S, BRCM7356, BRCM7424, BRCM7425, BRCM7435, BRCM7444, BRCM7552, BRCM7581, BRCM7583, BRCM7584, BRCM72604, BRCM75845, BRCM7366, BRCM73625, BRCM73565, BRCM7439, HISIL_ARM} stb_type = UNKNOWN;
 #else
-static enum {UNKNOWN, WETEK, AZBOX863x, AZBOX865x, ST, PALLAS, VULCAN, XILLEON, BRCM7400, BRCM7401, BRCM7405, BRCM7325, BRCM7335, BRCM7346, BRCM7358, BRCM7362, BRCM7241, BRCM7251, BRCM7252, BRCM7252S, BRCM7356, BRCM7424, BRCM7425, BRCM7435, BRCM7444, BRCM7552, BRCM7581, BRCM7583, BRCM7584, BRCM72604, BRCM75845, BRCM7366, BRCM73625, BRCM73565, BRCM7439} stb_type = UNKNOWN;
+static enum {UNKNOWN, WETEK, AZBOX863x, AZBOX865x, ST, PALLAS, VULCAN, XILLEON, BRCM7400, BRCM7401, BRCM7405, BRCM7325, BRCM7335, BRCM7346, BRCM7358, BRCM7362, BRCM7241, BRCM7251, BRCM7252, BRCM7252S, BRCM7356, BRCM7424, BRCM7425, BRCM7435, BRCM7444, BRCM7552, BRCM7581, BRCM7583, BRCM7584, BRCM72604, BRCM75845, BRCM7366, BRCM73625, BRCM73565, BRCM7439, HISIL_ARM} stb_type = UNKNOWN;
 #endif
 
 static int chr_luma_stride = 0x40;
@@ -359,6 +359,11 @@ int main(int argc, char **argv)
 				else if (strstr(buf,"7376"))
 				{
 					stb_type = BRCM7366;
+					break;
+				}
+				else if (strstr(buf,"hi3798"))
+				{
+					stb_type = HISIL_ARM;
 					break;
 				}
 				else if (strstr(buf,"Meson-6") || strstr(buf,"Meson-64"))
@@ -589,7 +594,7 @@ int main(int argc, char **argv)
 	{
 		if (!quiet)
 			fprintf(stderr, "Grabbing Video ...\n");
-		if (stb_type == BRCM7366 || stb_type == BRCM7251 || stb_type == BRCM7252 || stb_type == BRCM7252S || stb_type == BRCM7444 || stb_type == BRCM72604)
+		if (stb_type == BRCM7366 || stb_type == BRCM7251 || stb_type == BRCM7252 || stb_type == BRCM7252S || stb_type == BRCM7444 || stb_type == BRCM72604 || stb_type == HISIL_ARM)
 		{
 			getvideo2(video, &xres_v,&yres_v);
 		}
